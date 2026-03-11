14cb0e4be585adc9e58e3db07ca5fe042d23f072
HasnathAhmedTamim <hasnath.tamim333@gmail.com>
2026-03-11 13:43:27 +0600

    data add

---
+++ src/data/tournaments.ts
@@ -0,0 +1,22 @@
+import { Tournament } from "@/types/tournament";
+
+export const tournaments: Tournament[] = [
+  {
+    id: "1",
+    name: "Weekend FC26 Elite Cup",
+    status: "Upcoming",
+    budget: 2000,
+    maxPlayers: 24,
+    minPlayers: 15,
+    participants: 8,
+  },
+  {
+    id: "2",
+    name: "Friends Auction League",
+    status: "Live",
+    budget: 1800,
+    maxPlayers: 22,
+    minPlayers: 15,
+    participants: 10,
+  },
+];
