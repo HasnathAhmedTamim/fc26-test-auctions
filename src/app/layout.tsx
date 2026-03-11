48c17c74bc6770c6a4893f491dbc2af2f3040e86
HasnathAhmedTamim <hasnath.tamim333@gmail.com>
2026-03-11 12:45:01 +0600

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Navbar } from "@/components/layout/navbar";
-import { Footer } from "@/components/layout/footer";
 
 export const metadata: Metadata = {
   title: "FC26 Auction",
